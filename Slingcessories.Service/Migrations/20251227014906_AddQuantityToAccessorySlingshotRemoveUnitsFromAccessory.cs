ed705699b326c66f4d3c02a5f9b9fd91e46ba0e0
Edward Laiz <edwardlaiz@gmail.com>
2025-12-27 09:15:18 -0500

    Merge

---
+++ Slingcessories.Service/Migrations/20251227014906_AddQuantityToAccessorySlingshotRemoveUnitsFromAccessory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Slingcessories.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuantityToAccessorySlingshotRemoveUnitsFromAccessory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Units",
+                table: "Accessories");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "AccessorySlingshots",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "AccessorySlingshots");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Units",
+                table: "Accessories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
